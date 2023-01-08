b73ada62e54ab1ccc88a70bda62a7a178aa80112
Kotáb Petr <518-kotabpe@users.noreply.localhost>
2023-01-08 18:15:18 +0100

    Added shopping window.

--- TestGame/Scenes/ShopScene.cs
+++ TestGame/Scenes/ShopScene.cs
@@ -9,20 +9,76 @@ namespace TestGame.Scenes
 {
     internal class ShopScene : Scene
     {
+        private const int rowSize = 7;
+        private const int columnSize = 4;
+        private const float shopScale = 1.0f;
+
         private Inventory inventory;
+        private Vector2 windowSize;
 
         public void SetInvetory(Inventory inventory) => this.inventory = inventory;
+
+        public override void Initialize(LDGame game)
+        {
+            windowSize = new Vector2()
+            {
+                X = game.Graphics.PreferredBackBufferWidth,
+                Y = game.Graphics.PreferredBackBufferHeight,
+            };
+
+            base.Initialize(game);
+        }
 
         protected override void CreateUI()
         {
             CreateLeaveButton();
+            CreateShoppingWindow();
+        }
+
+        private void CreateShoppingWindow()
+        {
+            var tileApperance = new Apperance()
+            {
+                Scale = new Vector2(shopScale),
+                Sprite = new Sprite()
+                {
+                    Texture = Game.SpriteManager["scrollsandblocks"],
+                    SourceRectange = new Rectangle(96, 128, 96, 96),
+                    Color = new Color(1.0f, 1.0f, 1.0f, 0.7f),
+                },
+            };
+            tileApperance.Position = new Vector2()
+            {
+                X = windowSize.X / 2 - (tileApperance.Size.X * rowSize) / 2,
+                Y = windowSize.Y * 0.03f,
+            };
+
+            var font = Game.FontManager[new FontDescriptor()
+            {
+                Name = "calibri",
+                FontHeight = 16,
+            }];
+
+            for (int x = 0; x < rowSize; x++)
+            {
+                for (int y = 0; y < columnSize; y++)
+                {
+                    var apperance = tileApperance.Clone();
+                    apperance.Position += new Vector2(x, y) * apperance.Size;
+
+                    var button = new Button()
+                    {
+                        Apperance = apperance,
+                        Font = font,
+                        Text = "",
+                    };
+                    UILayer.AddElement(button);
+                }
+            }
         }
 
         private void CreateLeaveButton()
         {
-            float screenWidth = Game.Graphics.PreferredBackBufferWidth;
-            float screenHeight = Game.Graphics.PreferredBackBufferHeight;
-
             var button = new Button()
             {
                 Apperance = new Apperance()
@@ -43,8 +99,8 @@ namespace TestGame.Scenes
 
             button.Apperance.Position = new Vector2()
             {
-                X = screenWidth * 0.97f - button.Apperance.Size.X,
-                Y = screenHeight * 0.03f,
+                X = windowSize.X * 0.97f - button.Apperance.Size.X,
+                Y = windowSize.Y * 0.03f,
             };
 
             button.Clicked += (sender, e) => Game.PopScene();
