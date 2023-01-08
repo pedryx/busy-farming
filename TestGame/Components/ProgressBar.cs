f8c192988618a078f1bcc881f65fe4eca41ab880
Kotáb Petr <518-kotabpe@users.noreply.localhost>
2023-01-08 19:10:48 +0100

    Added progressbar component.

---
+++ TestGame/Components/ProgressBar.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+
+
+namespace TestGame.Components
+{
+    internal class ProgressBar
+    {
+        public float StartValue;
+        public float EndValue;
+        public float CurrentValue;
+        public Color Color;
+    }
+}
